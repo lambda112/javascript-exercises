bd7e4fbe34ccf3c1b7b2ac29194ef9d1dc42d7a4
Kamran Azim <Kamazim121212@gmail.com>
2026-08-20 18:41:28 +0100

    Completed data type and conditionals challanges

--- foundations/05_joinStrings/joinStrings.js
+++ foundations/05_joinStrings/joinStrings.js
@@ -7,10 +7,15 @@
  The greeting should read: "Hello! My name is Carlos Stevenson and I am 18 years old."
 */
 
+
 // Add your code right below, good luck!
-
-
-
+let firstName = "Carlos"
+let lastName = "Stevenson"
+let thisYear = 1965
+let birthYear = 1947
+let age = thisYear - birthYear
+let fullName = firstName + " " + lastName
+let greeting = `Hello! My name is ${fullName} and I am ${age} years old.`
 
 
 // Do not change this
